77a39af1509c5879573a1fcfe6a464b851dde87e
Jami L Johnson <jami.johnson@auckland.ac.nz>
2015-09-10 18:23:11 +1200

    update documentation

--- LeapMotion/LeapMotion_Driver.py
+++ LeapMotion/LeapMotion_Driver.py
@@ -1,3 +1,8 @@
+'''
+LeapMotion driver for acquiring hand information and gestures
+September 10, 2015
+@author: Jami L Johnson
+'''
 import Leap, sys, thread, time
 from Leap import SwipeGesture
 
@@ -6,18 +11,15 @@ class LeapListener(Leap.Listener):
         print 'LeapMotion initialized'
 
     def on_connect(self,controller):
-        print 'Connected'
+        print 'LeapMotion connected'
         controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
 
     def on_disconnect(self,controller):
         print 'LeapMotion disconnected'
 
     def on_exit(self,controller):
-        print 'Exited LeapMotion acquisition loop.'
+        print 'Exited LeapMotion.'
 
     def get_frame(self,controller):
         frame = controller.frame()
         return frame.hands, frame.gestures()
-
-
-    
