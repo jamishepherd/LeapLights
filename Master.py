6bd75b66d972eb46d74c91c6004e3e253bc3b2f5
tahaase <thaa191@aucklanduni.ac.nz>
2015-09-16 20:09:48 +1200

    Added dasy chain support for 2 lights by creating new class in the DMX_Driver.py

--- Master.py
+++ Master.py
@@ -20,21 +20,21 @@ def main():
         controller = Leap.Controller()
         controller.add_listener(leapDev)
         print "Connected to LeapMotion"
-        dmx_right = DMX_Driver.DMX_Controller("/dev/ttyUSB1",2,0,False)
-        dmx_left = DMX_Driver.DMX_Controller("/dev/ttyUSB0",2,4,True)
+#        dmx_right = DMX_Driver.DMX_Controller("/dev/ttyUSB0",1,0,False)
+#        dmx_left = DMX_Driver.DMX_Controller("/dev/ttyUSB1",1,4,True)
+        dmx_guy = DMX_Driver.DMX_Controller_Chained("/dev/ttyUSB0",1,0,100,4,False,True)
         print "Connected to Lights"
-
-        # initialize hand positions
-        left_position = Leap.Vector(1, 0.5, 0)
-        right_position = Leap.Vector(1, 0.5, 0)        
-        
-        # initialize grip strength
-        left_grip_strength = 0        
-        right_grip_strength = 0
             
         while go == True:
             
             all_hands, all_gestures  = leapDev.get_frame(controller)
+            # initialize hand positions
+            left_position = Leap.Vector(1, 0.5, 0)
+            right_position = Leap.Vector(1, 0.5, 0)        
+        
+            # initialize grip strength
+            left_grip_strength = 0        
+            right_grip_strength = 0
  
             for hand in all_hands:
                 if hand.is_left:
@@ -43,7 +43,8 @@ def main():
                   
                     if hand.palm_velocity[0] < -900:
                         print 'left swipe'
-                        dmx_left.change_color() 
+#                        dmx_left.change_color()
+                        dmx_guy.change_color(False)
                         time.sleep(0.1)
                 else:
                     right_position = hand.palm_position
@@ -51,15 +52,18 @@ def main():
 
                     if hand.palm_velocity[0] > 900:
                         print 'right swipe'
-                        dmx_right.change_color()
+#                        dmx_right.change_color()
+                        dmx_guy.change_color(True)
                         time.sleep(0.1)
             
-            dmx_right.update_position(right_position)
-            dmx_left.update_position(left_position)
+#            dmx_right.update_position(right_position)
+#            dmx_left.update_position(left_position)
             
-            dmx_right.light_intensity(right_grip_strength)
-            dmx_left.light_intensity(left_grip_strength)
+#            dmx_right.light_intensity(right_grip_strength)
+#            dmx_left.light_intensity(left_grip_strength)
             
+            dmx_guy.update_position(right_position,left_position)
+            dmx_guy.light_intensity(right_grip_strength,left_grip_strength)
     except KeyboardInterrupt:
         go = False
         print 'Exiting Script'
